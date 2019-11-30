b065a3fabc39fd94f69ed5689bd06db9fcae05a0
Jeffrey McKinley <jeffrey@synapsestudios.com>
2019-11-30 14:28:28 -0700

    about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,27 +1,25 @@
 import React from "react"
-import styled from "styled-components"
 import TopNavBar from "../components/TopNavBar"
+import SectionHeader from "../components/common/SectionHeader"
+import Description from "../components/common/Description"
+import ImageContainer from "../components/common/ImageContainer"
 
-const About = props => (
+const About = ({ path }) => (
   <div>
-    <TopNavBar pathName={props.path} />
-    <LandingText>
-      <p>Hey there.</p>
-      <p>
-        I’m Jenny, a web/app and graphic designer located in Arizona. I envision
-        web applications and marketing material that bring clients’ business
-        identities to life. And, yes, I <i>do</i> code!
-      </p>
-    </LandingText>
+    <TopNavBar pathName={path} />
+    <SectionHeader>About</SectionHeader>
+    <ImageContainer />
+    <Description>
+      I am a humor-loving gamer with a background in the arts. In my spare time,
+      I love to play video games, draw, paint miniatures, or dabble in creative
+      hobbies. I'm also an animal lover and like to spend time snuggling and
+      playing with my two cats.
+    </Description>
+    <Description>
+      When I'm not at home, I'm usually hanging out with my boyfriend and
+      friends or out enjoying nature while hiking or at a whildlife preserve.
+    </Description>
   </div>
 )
-
-const LandingText = styled.div`
-  font-family: Lato;
-  font-weight: 300;
-  display: inline-block;
-  margin: 3em 0em 0em 10em;
-  width: 32em;
-`
 
 export default About
