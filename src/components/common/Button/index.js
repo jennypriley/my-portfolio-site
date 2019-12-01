14f32c72889c10e878d4ab6fe60abd9e9ebb92bd
Jenny Riley <jenny.p.riley@gmail.com>
2019-11-30 17:25:23 -0700

    Added styling to Contact page

--- src/components/common/Button/index.js
+++ src/components/common/Button/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import styled from "styled-components"
+import { orange, white } from "../../../styles/colors"
 
 function Button({ children, onClick, ...props }) {
   return (
@@ -10,6 +11,27 @@ function Button({ children, onClick, ...props }) {
 }
 
 export default styled(Button)`
-  height: 50px;
-  width: 200px;
+  padding: 0.5em 5em;
+  background-color: ${orange};
+  border-radius: 0.25em;
+  border: 0em solid transparent;
+  color: ${white};
+  font-family: Lato;
+  font-weight: 500;
+  font-size: 1.5em;
+  margin: 1rem 10rem;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  outline: none;
+  &:hover {
+    cursor: pointer;
+    opacity: 0.75;
+    -o-transition: 0.2s;
+    -ms-transition: 0.2s;
+    -moz-transition: 0.2s;
+    -webkit-transition: 0.2s;
+    transition: 0.2s;
+  }
 `
