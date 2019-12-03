5ff4b58066ff651905344f0b0eed1aa6063569a0
Jenny Riley <jenny.p.riley@gmail.com>
2019-12-02 18:34:29 -0700

    new location

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import WebFont from "webfontloader"
-if (typeof window === "undefined") {
-  global.window = {}
-}
+
 WebFont.load({
   google: {
     families: ["Lato:300,400,700", "Roboto:300,400,700", "sans-serif"],
